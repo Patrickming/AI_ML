52d9ff2dc703eacba26d64a0dcb9b65acd0a5c6e
Patrickming <patrickmingx@gmail.com>
2023-11-29 23:25:30 +0800

    commit day02 -> KNN done

--- Machine_Learning/day2/day02_machine_learning.py
+++ Machine_Learning/day2/day02_machine_learning.py
@@ -23,6 +23,7 @@ def knn_iris():
     # 3）特征工程：标准化
     transfer = StandardScaler()
     x_train = transfer.fit_transform(x_train)
+    # 用训练集的均值和方差来转换测试集，使得测试集和训练集特征值具有同样的均值和方差
     x_test = transfer.transform(x_test)
 
     # 4）KNN算法预估器
@@ -31,12 +32,12 @@ def knn_iris():
 
     # 5）模型评估
     # 方法1：直接比对真实值和预测值
-    y_predict = estimator.predict(x_test)
+    y_predict = estimator.predict(x_test) # 根据测试集的特征值数据预测目标值
     print("y_predict:\n", y_predict)
-    print("直接比对真实值和预测值:\n", y_test == y_predict)
+    print("直接比对真实值和预测值:\n", y_test == y_predict) # 直接拿预测出的目标值 和 测试集的目标值比对
 
     # 方法2：计算准确率
-    score = estimator.score(x_test, y_test)
+    score = estimator.score(x_test, y_test) # 拿训练好的estimator进行预测x_test -> y_test
     print("准确率为：\n", score)
 
     return None
@@ -80,7 +81,7 @@ def knn_iris_gscv():
     # 最佳参数：best_params_
     print("最佳参数：\n", estimator.best_params_)
     # 最佳结果：best_score_
-    print("最佳结果：\n", estimator.best_score_)
+    print("最佳结果：\n", estimator.best_score_) # 准确率和最佳结果不一样 最终还是看准确率 最佳结果只是进行交叉验证里验证集和训练集之间最好的
     # 最佳估计器：best_estimator_
     print("最佳估计器:\n", estimator.best_estimator_)
     # 交叉验证结果：cv_results_
@@ -156,8 +157,8 @@ if __name__ == "__main__":
     # 代码1： 用KNN算法对鸢尾花进行分类
     # knn_iris()
     # 代码2：用KNN算法对鸢尾花进行分类，添加网格搜索和交叉验证
-    # knn_iris_gscv()
+    knn_iris_gscv()
     # 代码3：用朴素贝叶斯算法对新闻进行分类
     # nb_news()
     # 代码4：用决策树对鸢尾花进行分类
-    decision_iris()
+    # decision_iris()
